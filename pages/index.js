177589ffa2278fa9dd7bdefb12b4c027b74126d6
lauchaventura <lautaroventura02@gmail.com>
2022-02-09 16:03:22 -0300

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import Head from 'next/head'
-import SearchBox from '../components/SearchBox'
-import FamousPlaces from '../components/FamousPlaces'
+import Head from "next/head";
+import SearchBox from "../components/SearchBox";
+import FamousPlaces from "../components/FamousPlaces";
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clima App</title>
-
+        <title>Weather App - Next</title>
       </Head>
 
       <div className="home">
         <div className="container">
-          <SearchBox placeholder="Search for a city" />
+          <SearchBox placeholder="Search for a city..." />
+
           <FamousPlaces />
         </div>
       </div>
-
     </div>
-  )
+  );
 }
--- pages/location/[city].js
+++ pages/location/[city].js
@@ -9,98 +9,98 @@ import SearchBox from "../../components/SearchBox";
 import moment from "moment-timezone";
 
 export async function getServerSideProps(context) {
-    const city = getCityId(context.params.city);
+  const city = getCityId(context.params.city);
 
-    if (!city) {
-        return {
-            notFound: true,
-        };
-    }
-
-    const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&exclude=minutely&units=metric`
-    );
-
-    const data = await res.json();
-
-    if (!data) {
-        return {
-            notFound: true,
-        };
-    }
-
-    const hourlyWeather = getHourlyWeather(data.hourly, data.timezone);
-
-    const weeklyWeather = data.daily;
-
+  if (!city) {
     return {
-        props: {
-            city: city,
-            timezone: data.timezone,
-            currentWeather: data.current,
-            hourlyWeather: hourlyWeather,
-            weeklyWeather: weeklyWeather,
-        }, // will be passed to the page component as props
+      notFound: true,
     };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&exclude=minutely&units=metric`
+  );
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const hourlyWeather = getHourlyWeather(data.hourly, data.timezone);
+
+  const weeklyWeather = data.daily;
+
+  return {
+    props: {
+      city: city,
+      timezone: data.timezone,
+      currentWeather: data.current,
+      hourlyWeather: hourlyWeather,
+      weeklyWeather: weeklyWeather,
+    }, // will be passed to the page component as props
+  };
 }
 
 const getCityId = (param) => {
-    const cityParam = param.trim();
-    // get the id of the city
-    const splitCity = cityParam.split("-");
-    const id = splitCity[splitCity.length - 1];
+  const cityParam = param.trim();
+  // get the id of the city
+  const splitCity = cityParam.split("-");
+  const id = splitCity[splitCity.length - 1];
 
-    if (!id) {
-        return null;
-    }
+  if (!id) {
+    return null;
+  }
 
-    const city = cities.find((city) => city.id.toString() == id);
+  const city = cities.find((city) => city.id.toString() == id);
 
-    if (city) {
-        return city;
-    } else {
-        return null;
-    }
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
 };
 
 const getHourlyWeather = (hourlyData, timezone) => {
-    const endOfDay = moment().tz(timezone).endOf("day").valueOf();
-    const eodTimeStamp = Math.floor(endOfDay / 1000);
+  const endOfDay = moment().tz(timezone).endOf("day").valueOf();
+  const eodTimeStamp = Math.floor(endOfDay / 1000);
 
-    const todaysData = hourlyData.filter((data) => data.dt < eodTimeStamp);
+  const todaysData = hourlyData.filter((data) => data.dt < eodTimeStamp);
 
-    return todaysData;
+  return todaysData;
 };
 
 export default function City({
-    city,
-    weather,
-    currentWeather,
-    hourlyWeather,
-    weeklyWeather,
-    timezone,
+  city,
+  weather,
+  currentWeather,
+  hourlyWeather,
+  weeklyWeather,
+  timezone,
 }) {
-    return (
-        <>
-            <Head>
-                <title>{city.name} Weather - Next Weather App</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>{city.name} Weather - Next Weather App</title>
+      </Head>
 
-            <div className="page-wrapper">
-                <div className="container">
-                    <Link href="/">
-                        <a className="back-link">&larr; Home</a>
-                    </Link>
-                    <SearchBox placeholder="Search for another location..." />
-                    <TodaysWeather
-                        city={city}
-                        weather={weeklyWeather[0]}
-                        timezone={timezone}
-                    />
-                    <HourlyWeather hourlyWeather={hourlyWeather} timezone={timezone} />
-                    <WeeklyWeather weeklyWeather={weeklyWeather} timezone={timezone} />
-                </div>
-            </div>
-        </>
-    );
+      <div className="page-wrapper">
+        <div className="container">
+          <Link href="/">
+            <a className="back-link">&larr; Home</a>
+          </Link>
+          <SearchBox placeholder="Search for another location..." />
+          <TodaysWeather
+            city={city}
+            weather={weeklyWeather[0]}
+            timezone={timezone}
+          />
+          <HourlyWeather hourlyWeather={hourlyWeather} timezone={timezone} />
+          <WeeklyWeather weeklyWeather={weeklyWeather} timezone={timezone} />
+        </div>
+      </div>
+    </>
+  );
 }
